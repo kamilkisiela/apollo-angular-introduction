9f04c4e6246aea53dc1c49fe95378941e5465ea3
Kamil Kisiela <kamil.kisiela@gmail.com>
2018-09-01 20:36:05 +0200

    Step 2: Set graphql endpoint
    One thing you need to do is to define a graphql endpoint, an adress to your graphql server. I already created one so you can just copy paste it.

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -3,7 +3,8 @@ import {ApolloModule, APOLLO_OPTIONS} from 'apollo-angular';
 import {HttpLinkModule, HttpLink} from 'apollo-angular-link-http';
 import {InMemoryCache} from 'apollo-cache-inmemory';
 
-const uri = ''; // <-- add the URL of the GraphQL server here
+const uri = 'https://simple-posts-authors-graphql.glitch.me';
+
 export function createApollo(httpLink: HttpLink) {
   return {
     link: httpLink.create({uri}),
