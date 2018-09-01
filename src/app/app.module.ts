a4d693d7f3867ce679c2b7fba6d43c7d9636dcbc
Kamil Kisiela <kamil.kisiela@gmail.com>
2018-09-01 20:35:19 +0200

    Step 1: ng add apollo-angular
    Apollo Angular supports Angular's Schematics.
    
    By running 'ng add apollo-angular' you will have your project prepared for Apollo!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ListComponent } from './list.component';
 import { UpvoterComponent } from './upvoter.component';
+import { GraphQLModule } from './graphql.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, ListComponent, UpvoterComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, GraphQLModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
